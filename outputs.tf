d2e27ea6a6cdd981a8c26384f5af81962587eaea
Douglass Coronado <dcoronado@funio.com>
2023-09-18 21:04:45 -0400

    add sensitive attribute in outputs

--- outputs.tf
+++ outputs.tf
@@ -3,21 +3,26 @@ output "id" {
 }
 
 output "kube_config" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.myk8s.kube_config_raw
 }
 
 output "client_key" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.myk8s.kube_config.0.client_key
 }
 
 output "client_certificate" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.myk8s.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.myk8s.kube_config.0.cluster_ca_certificate
 }
 
 output "host" {
+  sensitive = true
   value = azurerm_kubernetes_cluster.myk8s.kube_config.0.host
 }
