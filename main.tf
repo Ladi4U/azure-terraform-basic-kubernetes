89ed9f58a5960098749b68ae9ee07d28cd73d247
Douglass Coronado <dcoronado@funio.com>
2023-09-18 20:30:44 -0400

    modify name definition

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "mcit" {
-  name     = "rg-${convention}"
+  name     = "rg-${var.convention}"
   location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "myk8s" {
-  name                = "k8s-${convention}"
+  name                = "k8s-${var.convention}"
   location            = azurerm_resource_group.myk8s.location
   resource_group_name = azurerm_resource_group.myk8s.name
   dns_prefix          = "${var.prefix}-k8s"
