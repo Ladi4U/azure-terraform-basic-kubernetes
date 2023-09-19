49a4b9672da6394918fe1ac6dd81097a57311944
Douglass Coronado <dcoronado@funio.com>
2023-09-18 22:12:46 -0400

    change index

--- outputs.tf
+++ outputs.tf
@@ -24,5 +24,5 @@ output "cluster_ca_certificate" {
 
 output "host" {
   sensitive = true
-  value = azurerm_kubernetes_cluster.myk8s["Pacific"].kube_config.0.host
+  value = azurerm_kubernetes_cluster.myk8s["*"].kube_config.0.host
 }
