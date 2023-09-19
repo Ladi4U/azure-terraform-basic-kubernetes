e3d4b2e9a4107475e2fa143373d7f5aa36b243e7
Douglass Coronado <dcoronado@funio.com>
2023-09-18 21:30:34 -0400

    fix outputs variables

--- outputs.tf
+++ outputs.tf
@@ -4,25 +4,25 @@ output "id" {
 
 output "kube_config" {
   sensitive = true
-  value = azurerm_kubernetes_cluster.myk8s[each.key].kube_config_raw
+  value = azurerm_kubernetes_cluster.myk8s[*].kube_config_raw
 }
 
 output "client_key" {
   sensitive = true
-  value = azurerm_kubernetes_cluster.myk8s[each.key].kube_config.0.client_key
+  value = azurerm_kubernetes_cluster.myk8s[*].kube_config.0.client_key
 }
 
 output "client_certificate" {
   sensitive = true
-  value = azurerm_kubernetes_cluster.myk8s[each.key].kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.myk8s[*].kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
   sensitive = true
-  value = azurerm_kubernetes_cluster.myk8s[each.key].kube_config.0.cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.myk8s[*].kube_config.0.cluster_ca_certificate
 }
 
 output "host" {
   sensitive = true
-  value = azurerm_kubernetes_cluster.myk8s[each.key].kube_config.0.host
+  value = azurerm_kubernetes_cluster.myk8s[*].kube_config.0.host
 }
